e3199e5250d4ed76145b139523031b6cf7a1e638
Ansore <ansore.b.reisu@gmail.com>
2015-10-24 12:47:24 +0100

    Add Class Parameters, Create Constructors and Implement a couple of update functionalities

--- Workshop.cpp
+++ Workshop.cpp
@@ -3,3 +3,55 @@
 //
 
 #include "Workshop.h"
+#include "Utilities.h.h"
+
+bool Workshop::addClient(Client client) {
+    if(isClient(client))
+        return false;
+    clients.push_back(client);
+    clients[clients.size()-1].setClientID(clients.size()-1);
+    return true;
+}
+
+bool Workshop::addEmployee(Employee employee) {
+    if(isEmployee(employee))
+        return false;
+    employees.push_back(employee);
+    employees[employees.size()-1].setEmployeeID(employees.size()-1);
+    return true;
+}
+
+void Client::setClientID(int clientID) {
+    this->clientID = clientID;
+}
+
+int Client::getClientID() const {
+    return clientID;
+}
+
+Client::Client(string name) {
+    this->name = name;
+    this->clientID = -1;
+}
+
+bool Workshop::isClient(Client client1) {
+    return exists(client1, clients);
+}
+
+bool Workshop::isEmployee(Employee employee1){
+    return exists(employee1, employees);
+}
+
+Employee::Employee(string name) {
+    this->name = name;
+    this->employeeID = -1;
+}
+
+int Employee::getEmployeeID() const {
+    return this->employeeID;
+}
+
+void Employee::setEmployeeID(int employeeID) {
+    this->employeeID = employeeID;
+}
+
--- Workshop.h
+++ Workshop.h
@@ -17,25 +17,34 @@ class Workshop {
     vector<Employee> employees;
     vector<Client> clients;
 public:
-    Workshop();
+    Workshop(){};
+    bool isClient(Client client1);
+    bool isEmployee(Employee employee1);
+    bool addClient(Client client); //bool needed ?
+    bool addEmployee(Employee employee); //bool needed ?
+    bool addVehicle(Vehicle* vehicle); //bool needed ?
 };
 
 class Employee {
     string name;
+    int employeeID;
     vector<Vehicle *> vehicles;
 public:
     Employee(string name);
-
     bool assignVehicle(Vehicle *v); //bool needed ?
+    int getEmployeeID() const;
+    void setEmployeeID(int employeeID);
 };
 
 class Client {
     string name;
+    int clientID;
     vector<Vehicle *> vehicles;
 public:
     Client(string name);
-
     bool addVehicle(Vehicle *v); //bool needed?
+    int getClientID() const;
+    void setClientID(int clientID);
 };
 
 class Service {
