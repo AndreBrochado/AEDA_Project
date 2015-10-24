8f698f83460796d27d893a4d0a26a0e8ebee0ad8
Ansore <ansore.b.reisu@gmail.com>
2015-10-24 12:44:09 +0100

    Create Utility (search and compare) Template functions

---
+++ Utilities.h
@@ -0,0 +1,31 @@
+//
+// Created by up201403057 on 24-10-2015.
+//
+
+#ifndef AEDA_PROJECT_UTILITIES_H
+#define AEDA_PROJECT_UTILITIES_H
+
+#include <iostream>
+#include <vector>
+#include "Workshop.h"
+
+using namespace std;
+
+template<class T>
+bool exists(T obj, vector<T> v){
+    for(size_t i = 0; i < v.size(); i++){
+        if(obj == v[i])
+            return true;
+    }
+    return false;
+}
+
+bool operator==(const Client &c1,const Client &c2){
+    return c1.getClientID() == c2.getClientID();
+}
+
+bool operator==(const Employee &e1, const Employee &e2){
+    return e1.getEmployeeID() == e2.getEmployeeID();
+}
+
+#endif //AEDA_PROJECT_UTILITIES_H
